0d7c6514fd7ba2577fce0c4ec21282086d8a0314
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-18 23:53:50 +0100

    toward a more readable code, but less compact

--- code.php
+++ code.php
@@ -1,16 +1,31 @@
 <?php
-function expose($filename){
-echo "// <a href=\"?filename=$filename\">$filename</a>\n";
-echo htmlspecialchars(file_get_contents($filename));
-echo "// end of : $filename\n\n\n";
+
+// validity for safety (well chosen)
+$valid_filenames = ["login.php", "protected.php", "shell.php",
+    "upload.php", "text-edit.php", "code.php"];
+
+$filename = $_REQUEST["filename"]; // from expose() <a> link tag
+if( in_array($filename, $valid_filenames) ) {
+
+    // $filename is code-injection safe (being in validity array)
+
+    // as plain text
+    header("Content-Type: text/plain");
+    echo file_get_contents( $filename );
+
+}else{
+
+    // as HTML text
+    function expose($filename){ // $filename is code-injection safe (being used only from safe foreach)
+        echo "// <a href='?filename=$filename' >$filename</a>\n";
+        echo htmlspecialchars( file_get_contents( $filename ) );
+        echo "// end of : $filename\n\n\n";
+    }
+
+    echo "<pre>";
+    foreach($valid_filenames as $filename){
+        // $filename is code-injection safe (being taken from $valid_filenames)
+        expose($filename);
+    }
+    echo "</pre>";
 }
-
-$valid_filenames=["login.php","protected.php","shell.php","upload.php","text-edit.php","code.php"];
-
-if(in_array($_REQUEST["filename"],$valid_filenames)){
-header("Content-Type: text/plain");
-die(file_get_contents($_REQUEST["filename"]));}
-echo "<pre>";
-foreach($valid_filenames as $filename)
-expose($filename);
-echo "</pre>";
