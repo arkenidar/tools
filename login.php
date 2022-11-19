bf2671b01fb3368b8da1462d4897c5041172cac0
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-19 14:40:21 +0100

    file "login.php": login/logout with password (better code format and code comments)

--- login.php
+++ login.php
@@ -1,19 +1,27 @@
-<?php session_start();
+<?php
+
+// file for passwords
 include "secrets.php";
 if(!isset($secret_password)) die("error: missing a secrets.php file!");
+
+// session variable for login/logout from login/logout form
+session_start();
 if($_REQUEST["submit"]=="login" && $_REQUEST["passwd"]==$secret_password){
-$_SESSION["loggedin"]=true;
+    $_SESSION["loggedin"]=true;
 }
 if($_REQUEST["submit"]=="logout"){
-$_SESSION["loggedin"]=false;
+     $_SESSION["loggedin"]=false;
 }
+
 ?><!doctype html>
 <title>login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
+
+<!-- login/logout form -->
 Status: <?=$_SESSION["loggedin"]?"logged in":"logged out"?>
 <form action="" method="post">
-<input type="password" name="passwd" size="7"> <br>
-<input type="submit" value="login" name="submit">
-<input type="submit" value="logout" name="submit">
+    <input type="password" name="passwd" size="7"> <br>
+    <input type="submit" value="login" name="submit">
+    <input type="submit" value="logout" name="submit">
 </form>
