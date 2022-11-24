88020578e85645e79d105d8ffbce10b6b7a90247
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-24 15:40:02 +0100

    improved index.php "contents listing"

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-
-<title>directory contents listing</title>
+<?php
+$dir = explode('?', $_SERVER['REQUEST_URI'])[0];
+?><title><?= $dir ?> directory contents listing</title>
 <meta name="viewport" content="width=device-width">
 
 <?php
 
+print("<ul>\n");
 foreach ( scandir('.') as $entry ){
     if($entry[0]==".") continue;
-    print("<a href='$entry'>$entry</a><br>");
+    if($entry=="index.php") continue;
+
+    print("<li><a href='$entry' target='_blank' >$entry</a></li>\n");
 }
+print("</ul>\n");
